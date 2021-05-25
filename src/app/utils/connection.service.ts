ff52b8a327c38ae03a0c5906fcf1caf30c274744
Adam97 <adamsors97@gmail.com>
2021-05-25 20:30:27 +0200

    its a commit ment 2

--- src/app/utils/connection.service.ts
+++ src/app/utils/connection.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  greet() {
+    return this.http.get(environment.serverUrl, {responseType: 'text', withCredentials: true}); //aszinkronitas
+  }
 }
